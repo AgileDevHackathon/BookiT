e81e333761513db39a443bc414f0c6e56be6c654
Mike Bobadilla <mike.c.bobadilla@gmail.com>
2016-09-03 22:02:48 -0700

    user login functionality

---
+++ server/service-config.js
@@ -0,0 +1,21 @@
+Meteor.startup(function(){
+    ServiceConfiguration.configurations.upsert(
+      { service: "facebook" },
+      {
+        $set: {
+          appId: Meteor.settings.facebook.appId,
+          secret: Meteor.settings.facebook.secret
+        }
+      }
+    );
+    ServiceConfiguration.configurations.upsert(
+      { service: "google" },
+      {
+        $set: {
+          clientId: Meteor.settings.google.clientId,
+          secret: Meteor.settings.google.secret
+        }
+      }
+    );
+
+});
