6e946cbb0eeca777f7ba30b4cf860e8e72b12fb2
d h <dejhuey@gmail.com>
2016-09-04 21:24:28 -0700

    - Added protection designator to template names - Added initial basic routes for Trello "Pages" card pages

--- client/router.js
+++ client/router.js
@@ -1,5 +1,40 @@
+// Public routes
 FlowRouter.route('/', {
     action: function(){
-        BlazeLayout.render('main', {content: "home"})
-    }
-})
+        BlazeLayout.render('main', {content: 'public_home'})
+    },
+});
+FlowRouter.route('/login', {
+	action: function() {
+		BlazeLayout.render('main', {content: 'public_login'})
+	},
+});
+FlowRouter.route('/search_business', {
+	action: function() {
+		BlazeLayout.render('main', {content: 'public_search_business'})
+	},
+});
+FlowRouter.route('/signup', {
+	action: function() {
+		BlazeLayout.render('main', {content: 'public_signup'})
+	},
+});
+
+// User routes
+FlowRouter.route('/user/profile', {
+	action: function() {
+		BlazeLayout.render('main', {content: 'user_profile'})
+	},
+});
+FlowRouter.route('/user/select_business', {
+	action: function() {
+		BlazeLayout.render('main', {content: 'user_select_business'})
+	},
+});
+
+// Business routes
+FlowRouter.route('/business/dashboard', {
+	action: function() {
+		BlazeLayout.render('main', {content: 'business_dashboard'})
+	},
+});
