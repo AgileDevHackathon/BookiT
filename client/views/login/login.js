db0e6d908167a052838d53d082dfd42ac010dcac
Mike Bobadilla <mike.c.bobadilla@gmail.com>
2016-09-04 18:03:44 -0700

    login page broken up

---
+++ client/views/login/login.js
@@ -0,0 +1,54 @@
+Template.login.events({
+
+	"click .loginOrText": function () {
+
+		Router.go("/login");
+
+	},
+
+	"click .login": function (e, t) {
+		let name = $('input[name="name"]').val()
+		let email = $('input[name="email"]').val()
+		let password = $('input[name="password"]').val()
+		if(!email || !password || !name){
+			sweetAlert("Login Error", "Please enter all fields", "error")
+		} else {
+			Accounts.createUser({
+				email: email,
+				password: password,
+				profile: {
+					name: name
+				}
+			}, function(e){
+				if(e){
+					sweetAlert("Login Error", e.reason, "error")
+				} else {
+					Router.go("/profile")
+				}
+			})
+		}
+	},
+
+	"click .facebook": function(e, t){
+		Meteor.loginWithFacebook({
+			requestPermissions: ['user_friends', 'public_profile', 'email']
+		}, function(err){
+			if(err){
+				sweetAlert("Login Error", err.reason, "error")
+			} else {
+				Router.go("/profile")
+			}
+		})
+	},
+
+	"click .google": function(e, t){
+		Meteor.loginWithGoogle({}, function(err){
+			if(err){
+				sweetAlert("Login Error", err.reason, "error")
+			} else {
+				Router.go("/profile")
+			}
+		})
+	}
+
+});
